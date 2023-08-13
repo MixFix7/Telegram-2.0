9d01c87c0811daff473c5d905c7e0f9d9f070fc1
MixFix7 <mashtalar95@gmail.com>
2023-08-13 20:17:18 +0300

    updating models.py and serializers.py

--- backend/chats/models.py
+++ backend/chats/models.py
@@ -12,4 +12,3 @@ class Chat(models.Model):
 
 
 
-    
--- backend/message/serializers.py
+++ backend/message/serializers.py
@@ -1,8 +1,12 @@
 from rest_framework import serializers
 from .models import Message
+from chats.serializers import InterlocutorSerializer
+import datetime
 
 
 class MessageSerializer(serializers.ModelSerializer):
+    sender = InterlocutorSerializer()
+
     class Meta:
         model = Message
         fields = '__all__'
