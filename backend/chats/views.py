d6f166f049e6e08b8f65152f689d0da5feed8aea
MixFix7 <mashtalar95@gmail.com>
2023-08-14 16:28:47 +0300

    updating views.py and models.py

--- backend/chats/views.py
+++ backend/chats/views.py
@@ -23,22 +23,23 @@ class GetAllUserChatsAndMessages(APIView):
         try:
             username = request.data.get('username')
 
-            all_chats_and_all_messages = []
-            all_chats = Chat.objects.filter(Q(interlocutor1_username=username) | Q(interlocutor2_name=username))
+            all_chats_with_last_message = []
+            all_chats = Chat.objects.filter(Q(interlocutor1__username=username) | Q(interlocutor2__username=username))
 
             for chat in all_chats:
-                chat_messages = Message.objects.filter(chat=chat)
+                last_message = Message.objects.filter(chat=chat)
 
                 chat_json = ChatSerializer(chat).data
-                chat_messages_json = MessageSerializer(chat_messages, many=True).data
-                chat_json['messages'] = chat_messages_json
+                messages_json = MessageSerializer(last_message, many=True).data
+                chat_json['messages'] = messages_json
+                chat_json['last_message'] = messages_json[-1]
 
-                all_chats_and_all_messages.append(chat_json)
+                all_chats_with_last_message.append(chat_json)
 
-            return Response(all_chats_and_all_messages, status=status.HTTP_200_OK)
+            return Response(all_chats_with_last_message, status=status.HTTP_200_OK)
 
         except Exception as e:
-            return Response({'message': e})
+            return Response({'message': str(e)})
 
 
 class GetAllUserChats(APIView):
--- backend/message/models.py
+++ backend/message/models.py
@@ -1,12 +1,13 @@
 from django.contrib.auth.models import User
 from django.db import models
 from chats.models import Chat
+from datetime import timedelta, datetime
 
 
 class Message(models.Model):
     sender = models.ForeignKey(User, related_name='message', on_delete=models.SET_NULL, null=True)
     chat = models.ForeignKey(Chat, related_name='chat_message', on_delete=models.CASCADE, null=True)
-    dispatch_date = models.DateTimeField(auto_now_add=True)
+    dispatch_date = models.DateTimeField(blank=True)
     type = models.CharField(max_length=10, default='Unknown')
 
     text = models.TextField(default='', null=True, blank=True)
@@ -15,6 +16,12 @@ class Message(models.Model):
 
     def __str__(self):
         return f"Sender: {self.sender} to: {self.chat} {self.type}:  date: {self.dispatch_date}"
+
+    def save(self, *args, **kwargs):
+        if not self.dispatch_date:
+            self.dispatch_date = datetime.now()
+        super().save(*args, **kwargs)
+
 
 
 
