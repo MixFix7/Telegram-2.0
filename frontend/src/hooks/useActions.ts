2a29dc1f2b77c8d673967b327da408c08e182308
MixFix7 <mashtalar95@gmail.com>
2023-08-19 18:19:06 +0300

    register a searchChatsSlice in the store.ts

--- frontend/src/hooks/useActions.ts
+++ frontend/src/hooks/useActions.ts
@@ -5,12 +5,14 @@ import * as ChatsSliceActions from '../store/chats/chats.actions'
 import {actions as viewChatActions} from '../store/viewChat/viewChat.slice'
 import {actions as ChatsSliceActions2} from '../store/chats/chats.slice'
 import {actions as websocketActions}  from '../store/websocket/websocket.slice'
+import { searchChatSlice } from '../store/searchChats/searchChat.slice'
 
 const rootActions = {
     ...ChatsSliceActions,
     ...viewChatActions,
     ...ChatsSliceActions2,
     ...websocketActions,
+    ...searchChatSlice.actions,
 }
 
 export const useActions = () => {
--- frontend/src/store/store.ts
+++ frontend/src/store/store.ts
@@ -2,11 +2,13 @@ import { configureStore, combineReducers} from "@reduxjs/toolkit";
 import { chatsSlice } from "./chats/chats.slice";
 import { reducer as viewChatReducer } from "./viewChat/viewChat.slice"; // Змінено імпорт
 import { reducer as websocketReducer } from "./websocket/websocket.slice"; // Змінено імпорт
+import { searchChatSlice } from "./searchChats/searchChat.slice";
 
 const reducers = combineReducers({
     chats: chatsSlice.reducer,
     viewChat: viewChatReducer,
     websocket: websocketReducer,
+    searchChats: searchChatSlice.reducer,
 });
 
 export const store = configureStore({
